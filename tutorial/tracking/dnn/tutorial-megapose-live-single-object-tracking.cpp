b0ca8371eae8e52a644a38827d876a451877eef9
afonsocastro <afonsocastro@ua.pt>
2024-07-04 12:31:43 +0100

    Megapose server with socket communication (working!!) new wood_block.json file for new camera

--- tutorial/tracking/dnn/tutorial-megapose-live-single-object-tracking.cpp
+++ tutorial/tracking/dnn/tutorial-megapose-live-single-object-tracking.cpp
@@ -341,6 +341,40 @@ int main(int argc, const char *argv[])
   auto found = high_resolution_clock::now();
   auto mega_pose_init = high_resolution_clock::now();
 
+//  ------------------------------------------SOCKET----------------------------------------------------
+  int server_fd, new_socket, valread;
+  struct sockaddr_in address;
+  int opt = 1;
+  int addrlen = sizeof(address);
+  char buffer[1024] = {0};
+  // Creating socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+     perror("socket failed");
+     exit(EXIT_FAILURE);
+  }
+  // Forcefully attaching socket to the port 8080
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+     perror("setsockopt");
+     exit(EXIT_FAILURE);
+  }
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(8080);
+  // Forcefully attaching socket to the port 8080
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+     perror("bind failed");
+     exit(EXIT_FAILURE);
+  }
+  if (listen(server_fd, 3) < 0) {
+     perror("listen");
+     exit(EXIT_FAILURE);
+  }
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+     perror("accept");
+     exit(EXIT_FAILURE);
+  }
+//  ------------------------------------------SOCKET----------------------------------------------------
+
   //! [Acquisition]
   while (true) {
     const double frameStart = vpTime::measureTimeMs();
@@ -437,42 +471,22 @@ int main(int argc, const char *argv[])
 
     if (tracking) {
 //   ---------------------------------------------SOCKET---------------------------------------------------------------
-       int server_fd, new_socket, valread;
-       struct sockaddr_in address;
-       int opt = 1;
-       int addrlen = sizeof(address);
-       char buffer[1024] = {0};
-//       const char* hello = "Hello from server";
-       // Creating socket file descriptor
-       if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-           perror("socket failed");
-           exit(EXIT_FAILURE);
-       }
-       // Forcefully attaching socket to the port 8080
-       if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-           perror("setsockopt");
-           exit(EXIT_FAILURE);
-       }
-       address.sin_family = AF_INET;
-       address.sin_addr.s_addr = INADDR_ANY;
-       address.sin_port = htons(8080);
-       // Forcefully attaching socket to the port 8080
-       if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-           perror("bind failed");
-           exit(EXIT_FAILURE);
-       }
-       if (listen(server_fd, 3) < 0) {
-           perror("listen");
-           exit(EXIT_FAILURE);
-       }
-       if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-           perror("accept");
-           exit(EXIT_FAILURE);
-       }
-//       valread = read(new_socket, buffer, 1024);
-       printf("%s\n",buffer );
-       send(new_socket, megaposeEstimate.cTo, strlen(megaposeEstimate.cTo), 0);
-       printf("Hello message sent\n");
+      std::cout << "megaposeEstimate.cTo: " << megaposeEstimate.cTo << std::endl;
+      vpTranslationVector tv;
+      vpQuaternionVector q;
+
+      megaposeEstimate.cTo.extract(tv);
+      megaposeEstimate.cTo.extract(q);
+
+      std::string s = "T: " + std::to_string(tv[0]) + " " + std::to_string(tv[1]) + " " + std::to_string(tv[2]);
+      s.append("\nQ: " + std::to_string(q[0]) + " " + std::to_string(q[1]) + " " + std::to_string(q[2]) + " " + std::to_string(q[3]));
+
+      std::cout << "s: " << s << std::endl;
+
+      char* c = const_cast<char*>(s.c_str());
+//      valread = read( new_socket , buffer, 1024);
+      printf("%s\n",buffer );
+      send(new_socket , c , strlen(c) , 0 );
 //   ---------------------------------------------SOCKET---------------------------------------------------------------
 
       if (overlayModel) {
